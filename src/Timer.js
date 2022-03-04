09d607a53db262f074ded2cc7202d4c1204bf805
Manish Jha <manishjhazyx@gmail.com>
2022-03-04 15:26:08 +0530

    added progressbar and buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import PauseButton from "./PauseButton";
+import PlayButton from "./PlayButton";
+import ResetButton from "./ResetButton";
+
+const red = "rgb(229, 0, 0)";
+const green = "rgb(0, 229, 0)";
 
 function Timer() {
-  return <div>Timer</div>;
+  return (
+    <div>
+      <CircularProgressbar
+        value={60}
+        text={`60%`}
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          tailColor: "rgba(255, 255, 255, .2)",
+        })}
+      />
+      <div className="buttons" style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+        <ResetButton />
+      </div>
+    </div>
+  );
 }
 
 export default Timer;
